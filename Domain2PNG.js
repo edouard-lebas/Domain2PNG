a9e5e4c08e2935047a99dada690b40cf8244666a
edouard-lebas <edouard.lebas@outlook.com>
2021-03-29 13:37:59 +0200

    fix: date format & directory integration usage

--- Domain2PNG.js
+++ Domain2PNG.js
@@ -86,8 +86,22 @@ let hashPNG = [];
 /**
  * Generate current date with specific format
  */
-const today = new Date();
-const options = { year: "numeric", month: "numeric", day: "numeric" };
+let dt = new Date();
+
+let finaleDate = `${dt.getDate().toString().padStart(2, "0")}-${(
+  dt.getMonth() + 1
+)
+  .toString()
+  .padStart(2, "0")}-${dt
+  .getFullYear()
+  .toString()
+  .padStart(4, "0")}-${dt
+  .getHours()
+  .toString()
+  .padStart(2, "0")}-${dt
+  .getMinutes()
+  .toString()
+  .padStart(2, "0")}-${dt.getSeconds().toString().padStart(2, "0")}`;
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -120,8 +134,7 @@ const options = { year: "numeric", month: "numeric", day: "numeric" };
        * Local date string to FR version : yyyy-mm-dd
        * Create the folder if not exists
        */
-      const newDirecotryName =
-        "res/" + today.toLocaleDateString("fr-fr", options) + dirHistory;
+      const newDirecotryName = "res/" + finaleDate + dirHistory;
       fs.mkdirSync(newDirecotryName, { recursive: true });
       const newFileName = newDirecotryName + domain + ".png";
       await moveToAnotherFolder(filename, newFileName);
